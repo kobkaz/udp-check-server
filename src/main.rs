b9729504ab761476ea8456e25f49dc3e3dc33027
KOBAYASHI Kazuhiro <kobayashi-kazuhiro@arkedgespace.com>
2024-06-07 10:50:02 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+    let sock = tokio::net::UdpSocket::bind(("0.0.0.0", args.port)).await?;
+
+    let mut buf = [0; 65535];
+    loop {
+        let (len, addr) = sock.recv_from(&mut buf).await?;
+        let buf = &buf[..len];
+        println!("Received {} bytes from {}", buf.len(), addr);
+        sock.send_to(format!("Received {} bytes\n", buf.len()).as_bytes(), addr)
+            .await?;
+    }
+}
